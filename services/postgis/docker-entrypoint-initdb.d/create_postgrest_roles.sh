ee58f2d0167da48bd9afe4dc3232b78ba8478f99
Diego Quintana <daquintanav@gmail.com>
2023-09-03 18:59:12 +0200

    dev: add postgrest service
    this service maps automagically the tables  in schema `api` at postgis to a restful api.

---
+++ services/postgis/docker-entrypoint-initdb.d/create_postgrest_roles.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# Create roles for PostgREST
+# https://postgrest.org/en/stable/tutorials/tut0.html#step-4-create-database-for-api
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+	CREATE SCHEMA api;
+	CREATE ROLE web_anon NOLOGIN;
+	GRANT USAGE ON SCHEMA api TO web_anon;
+	GRANT SELECT ON ALL TABLES IN SCHEMA api TO web_anon;
+	CREATE ROLE authenticator NOINHERIT LOGIN PASSWORD '$PGRST_AUTHENTICATOR_PASSWORD';
+	GRANT web_anon to authenticator;
+EOSQL
