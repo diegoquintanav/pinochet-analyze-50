bbcd4864ae84eb48e604e8b927a9e7053514bd9e
Diego Quintana <daquintanav@gmail.com>
2023-09-04 23:38:44 +0200

    dev: update example query

--- services/postgrest/scripts/get_from_table.sh
+++ services/postgrest/scripts/get_from_table.sh
@@ -2,8 +2,9 @@
 
 # example postgrest query
 POSTGIS_TABLE="t_pinochet__construct_geometries"
-POSTGREST_ENDPOINT="http://localhost:3010"
+POSTGREST_ENDPOINT="http://localhost:3000"
 QUERY_PARAMS="?limit=2"
 
 echo "GET from $POSTGREST_ENDPOINT/$POSTGIS_TABLE$QUERY_PARAMS"
-curl $POSTGREST_ENDPOINT/$POSTGIS_TABLE$QUERY_PARAMS -X GET -H "Content-Type: application/json" | jq
+curl $POSTGREST_ENDPOINT/$POSTGIS_TABLE$QUERY_PARAMS -X GET -H "Content-Type: application/json"
+echo ""
