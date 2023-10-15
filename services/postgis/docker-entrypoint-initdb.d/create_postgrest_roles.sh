dff4d16010bc3ccb5d045c0d76ed1cdec05262b6
Diego Quintana <daquintanav@gmail.com>
2023-10-15 19:26:22 +0200

    dev: update postgis and python dependencies

--- services/postgis/docker-entrypoint-initdb.d/create_postgrest_roles.sh
+++ services/postgis/docker-entrypoint-initdb.d/create_postgrest_roles.sh
@@ -5,6 +5,7 @@ set -e
 # https://postgrest.org/en/stable/tutorials/tut0.html#step-4-create-database-for-api
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+	-- Create schema and roles for PostgREST
 	CREATE SCHEMA api;
 	CREATE ROLE web_anon NOLOGIN;
 	GRANT USAGE ON SCHEMA api TO web_anon;
