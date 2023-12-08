0fd1dd1c99fbac26277bb83e1639c799dfc96f77
Diego Quintana <daquintanav@gmail.com>
2023-12-08 19:50:02 +0100

    new: detach location model from attribute place and location_n

--- dbt_pinochet/models/datasets/api/api_pinochet__location.sql
+++ dbt_pinochet/models/datasets/api/api_pinochet__location.sql
@@ -1,28 +1,12 @@
 {{ config(materialize="table") }}
 
 SELECT DISTINCT
-    {{
-        dbt_utils.generate_surrogate_key(
-            [
-                "location_id",
-                "location_n",
-                "place",
-                "location",
-                "latitude",
-                "longitude",
-                "exact_coordinates",
-                "geometry",
-            ]
-        )
-    }} AS pk,
     b.{{ adapter.quote("location_id") }},
-    b.{{ adapter.quote("location_n") }},
-    b.{{ adapter.quote("place") }},
     b.{{ adapter.quote("location") }},
     b.{{ adapter.quote("latitude") }},
     b.{{ adapter.quote("longitude") }},
     b.{{ adapter.quote("exact_coordinates") }},
     b.{{ adapter.quote("geometry") }},
-    '4326' AS {{ adapter.quote("srid") }}
-FROM {{ ref("stg_pinochet__base") }} AS b
+    b.{{ adapter.quote("srid") }}
+FROM {{ ref("api_pinochet__location_and_events") }} AS b
 ORDER BY b.{{ adapter.quote("location_id") }}
