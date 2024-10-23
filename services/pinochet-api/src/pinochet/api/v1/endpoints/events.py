f66991083145bc5e807ebbb2395ac814397852bb
Diego Quintana <daquintanav@gmail.com>
2024-10-23 07:12:04 -0300

    new: remove geoalchemy and endpoint

--- services/pinochet-api/src/pinochet/api/v1/endpoints/events.py
+++ services/pinochet-api/src/pinochet/api/v1/endpoints/events.py
@@ -42,30 +42,3 @@ def get_event_by_id(event_id: int, db: Session = Depends(get_db)) -> Any:
         raise HTTPException(status_code=404, detail="Item not found")
 
     return event
-
-
-# @router.get(
-#     "/{event_id}/get_events_around",
-#     response_model=EventOut,
-#     status_code=201,
-# )
-# def get_events_by_event_id_and_radius(
-#     event_id: int,
-#     db: Session = Depends(get_db),
-#     radius_km: int = 10,
-#     limit: int = 10,
-# ) -> Any:
-#     """
-#     Get all events from the DB given an event id.
-#     """
-
-#     q = db.query(Event)
-
-#     event: Event = q.where(Event.event_id == event_id).one_or_none()
-
-#     if not event:
-#         raise HTTPException(status_code=404, detail="Item not found")
-
-#     events = event.get_events_around_self(db, radius=radius_km)
-
-#     return events
